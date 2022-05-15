becaaf34a84326868ab219b010622667cadef61e
sunnnkim <ksunju0610@naver.com>
2022-05-15 22:45:25 +0900

    [20220515][2] spring-security 설정

---
+++ src/main/java/com/sunju/springboot/starter/domain/user/Role.java
@@ -0,0 +1,18 @@
+package com.sunju.springboot.starter.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    /**
+     * Spring Security 에서는 권한코드에 항상 ROLE_ 이 앞에 있어야 함
+     */
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER","일반 사용자");
+
+    private final String key;
+    private final String title;
+}
