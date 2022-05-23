caefbe854eb4074a81f7ef993270ac8d5ad6193e
sunnnkim <ksunju0610@naver.com>
2022-05-23 23:43:55 +0900

    [202205223][01] longinUser 어노테이션 클래스 만들기

---
+++ src/main/java/com/sunju/springboot/starter/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.sunju.springboot.starter.config;
+
+import com.sunju.springboot.starter.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/com/sunju/springboot/starter/config/auth/LoginUser.java
+++ src/main/java/com/sunju/springboot/starter/config/auth/LoginUser.java
@@ -9,4 +9,11 @@ import java.lang.annotation.Target;
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LoginUser {
+    /*
+        @Target
+            - 이 어노노테이션이 생성될 수 있는 위치를 지정
+            - PARAMETER : 메소드의 파라미터로 선언된 객체에서만 사용할 수 있음
+        @interface
+            - 이 파일을 어노테이션 클래스로 지정함
+     */
 }
